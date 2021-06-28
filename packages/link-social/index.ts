61862f778b7acfab39cb0e13e82ec6f9c5386989
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-28 13:15:32 +0900

    add social links

---
+++ packages/link-social/index.ts
@@ -0,0 +1,22 @@
+export const url_youtube_main =
+  "https://www.youtube.com/channel/UCgJO5apXl_pXRfTxNrkbEBw";
+export const url_facebook_main = "https://facebook.com/grida.co";
+export const url_instagram_main = "https://instagram.com/grida.co";
+export const url_twitter_main = "https://twitter.com/grida_co";
+export const url_github_main = "https://github.com/gridaco";
+/**
+ * update required
+ */
+export const url_dribbble_main = "https://dribbble.com/bridged";
+export const url_medium_main = "https://blog.grida.co";
+export const url_blog_main = url_medium_main;
+
+export const SociallUrls = {
+  youtube: url_youtube_main,
+  facebook: url_facebook_main,
+  instagram: url_instagram_main,
+  twitter: url_twitter_main,
+  github: url_github_main,
+  dribbble: url_dribbble_main,
+  medium: url_medium_main,
+};
