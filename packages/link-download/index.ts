d8a3628cb2ab4f29ebe617671752112746b14393
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-28 13:25:54 +0900

    ch tsconig

---
+++ packages/link-download/index.ts
@@ -0,0 +1,8 @@
+export const DownloadUrls = {
+  download_figma_plugin:
+    "https://www.figma.com/community/plugin/896445082033423994",
+  download_sketch_plugin: "https://github.com/gridaco/assistant/releases",
+  download_xd_plugin: "https://github.com/gridaco/assistant/releases",
+  download_vscoode_extension: "https://github.com/gridaco/vscode-extension/",
+  download_desktop_app: "https://github.com/gridaco/grida/releases",
+};
